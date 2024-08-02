1f7a62c472b80dce96fe9ac447460d4c48dcb217
Manan Gulati <manangulati9@gmail.com>
2024-08-02 20:26:40 +0530

    added filters and updated ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import { HomeForm } from "@/components/HomeForm";
 
 export default function Home() {
 	return (
-		<main>
+		<main className="flex flex-col gap-24 items-center p-36">
+			<h1 className="text-4xl font-bold underline text-primary">
+				Bajaj Finserv Health Dev Challenge
+			</h1>
 			<HomeForm />
 		</main>
 	);
--- src/components/HomeForm.tsx
+++ src/components/HomeForm.tsx
@@ -4,19 +4,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
@@ -24,6 +21,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { Checkbox } from "./ui/checkbox";
+import { toast } from "sonner";
 
 const formSchema = z.object({
 	input: z.string().startsWith("{").endsWith("}").includes("data"),
@@ -41,6 +40,10 @@ type TData = {
 
 export function HomeForm() {
 	const [data, setData] = useState<TData>();
+	const [showNums, setShowNums] = useState(true);
+	const [showAlphas, setShowAlphas] = useState(true);
+	const [showHighestAlpha, setShowHighestAlpha] = useState(true);
+
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
@@ -59,30 +62,39 @@ export function HomeForm() {
 	};
 
 	async function onSubmit(values: z.infer<typeof formSchema>) {
-		if (validateJsonString(values.input)) {
+		const result = validateJsonString(values.input);
+		if (result) {
 			const res = await fetch("/bfhl", {
 				method: "POST",
 				body: values.input,
 			});
-			const resData = await res.json();
-			setData(resData);
+			const resData = (await res.json()) as TData;
+
+			if (resData.is_success) {
+				setData(resData);
+				return;
+			}
+
+			toast.error("Unknown error occured!");
 		}
+
+		toast.error("Invalid JSON! Please check your input");
 	}
 
 	return (
 		<Form {...form}>
 			<form
 				onSubmit={form.handleSubmit(onSubmit)}
-				className="my-auto mx-auto space-y-8 max-w-7xl"
+				className="space-y-8 w-full max-w-7xl"
 			>
 				<FormField
 					control={form.control}
 					name="input"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>API Input</FormLabel>
+							<FormLabel className="text-lg font-semibold">API Input</FormLabel>
 							<FormControl>
-								<Input {...field} />
+								<Input placeholder={`{"data" : ["A", "1"]}`} {...field} />
 							</FormControl>
 							<FormMessage />
 						</FormItem>
@@ -92,18 +104,42 @@ export function HomeForm() {
 					Submit
 				</Button>
 				<DropdownMenu>
-					<DropdownMenuTrigger></DropdownMenuTrigger>
+					<DropdownMenuTrigger
+						className={buttonVariants({ variant: "default" })}
+					>
+						Filters
+					</DropdownMenuTrigger>
 					<DropdownMenuContent>
-						<DropdownMenuItem>Alphabets</DropdownMenuItem>
-						<DropdownMenuItem>Numbers</DropdownMenuItem>
-						<DropdownMenuItem>Highest Alphabet</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => setShowNums((prev) => !prev)}
+							className="flex gap-4 items-center"
+						>
+							<Checkbox checked={showNums} />
+							<p>Numbers</p>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => setShowAlphas((prev) => !prev)}
+							className="flex gap-4 items-center"
+						>
+							<Checkbox checked={showAlphas} />
+							<p>Alphabets</p>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => setShowHighestAlpha((prev) => !prev)}
+							className="flex gap-4 items-center"
+						>
+							<Checkbox checked={showHighestAlpha} />
+							<p>Highest Alphabet</p>
+						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 				<div>
-					<h2>Filtered Response</h2>
-					<div>Numbers: {data?.numbers.map((num) => `${num},`)}</div>
-					<div>Alphabets: {data?.alphabets.map((alpha) => `${alpha},`)}</div>
-					<div>Highest alphabet: {data?.highest_alphabet[0]}</div>
+					<h2 className="text-lg font-semibold">Filtered Response</h2>
+					{showNums && <div>• Numbers: {data?.numbers.join(", ")}</div>}
+					{showAlphas && <div>• Alphabets: {data?.alphabets.join(", ")}</div>}
+					{showHighestAlpha && (
+						<div>• Highest alphabet: {data?.highest_alphabet[0]}</div>
+					)}
 				</div>
 			</form>
 		</Form>
